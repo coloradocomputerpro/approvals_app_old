efe76992303d749722c4c6facd2bc18aa8575c77
jbressler <jbressler@pop-os.localdomain>
2023-03-03 02:14:38 -0700

    Fri Mar  3 02:14:38 AM MST 2023

--- approvals/models.py
+++ approvals/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Request(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=20, default='Pending')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.title
--- approvals/views.py
+++ approvals/views.py
@@ -7,66 +7,86 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login, logout
 
+from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
+from django.urls import reverse_lazy
+from .models import Request
+
+
 def login_view(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = AuthenticationForm(data=request.POST)
         if form.is_valid():
             user = form.get_user()
             login(request, user)
-            return redirect('index')
+            return redirect("index")
     else:
         form = AuthenticationForm()
-    return render(request, 'auth/login.html', {'form': form})
+    return render(request, "auth/login.html", {"form": form})
+
 
 from django.shortcuts import render, redirect
 from django.contrib.auth import logout
 
+
 def logout_view(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         logout(request)
-        return redirect('index')
+        return redirect("index")
     else:
         # If the request is not a POST request, just render the logout template
-        return render(request, 'auth/logout.html')
+        return render(request, "auth/logout.html")
+
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, "index.html")
+
 
 @login_required
 def manage_notices(request):
-    return render(request, 'manage_notices.html')
+    return render(request, "manage_notices.html")
+
 
 @user_passes_test(lambda u: u.is_superuser)
 def manage_users_groups(request):
-    return render(request, 'manage_users_groups.html')
+    return render(request, "manage_users_groups.html")
 
-from django.views.generic.edit import CreateView
-from django.urls import reverse_lazy
-from .models import Request
+
+def create(request):
+    # TODO: Implement create view
+    pass
+
 
 class RequestCreateView(CreateView):
     model = Request
-    fields = ['title', 'description']
-    template_name = 'approvals/request_create.html'
-    success_url = reverse_lazy('index')
+    fields = ["title", "description"]
+    template_name = "approvals/request_create.html"
+    success_url = reverse_lazy("index")
+
 
 from django.views.generic.detail import DetailView
 
+
 class RequestDetailView(DetailView):
     model = Request
-    template_name = 'approvals/request_detail.html'
+    template_name = "approvals/request_detail.html"
+
 
 from django.views.generic.edit import UpdateView
 
+
 class RequestUpdateView(UpdateView):
     model = Request
-    fields = ['title', 'description', 'status']
-    template_name = 'approvals/request_update.html'
-    success_url = reverse_lazy('index')
+    fields = ["title", "description", "status"]
+    template_name = "approvals/request_update.html"
+    success_url = reverse_lazy("index")
+
 
 from django.views.generic.list import ListView
 
+
 class RequestListView(ListView):
     model = Request
-    template_name = 'approvals/request_list.html'
-    context_object_name = 'requests'
+    template_name = "approvals/request_list.html"
+    context_object_name = "requests"
