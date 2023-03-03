800277809dca499216e87f95ccc3f6d69830c78e
jbressler <jbressler@pop-os.localdomain>
2023-03-03 00:47:30 -0700

    Fri Mar  3 12:47:30 AM MST 2023

--- pulse/urls.py
+++ pulse/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
-    path('', include('approvals.urls')),
-    path('approvals/', include('approvals.urls', namespace='approvals')),
+    path('', include('approvals.urls', namespace='approvals')),
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
     path("__reload__/", include("django_browser_reload.urls")),
