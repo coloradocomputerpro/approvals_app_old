cea74b0eec865f51b12195bbcb0cbaf7e7555771
jbressler <jbressler@pop-os.localdomain>
2023-03-03 17:37:47 -0700

    Fri Mar  3 05:37:47 PM MST 2023

--- approvals/models.py
+++ approvals/models.py
@@ -2,13 +2,20 @@ from django.db import models
 from django.urls import reverse
 from django.contrib.auth.models import User
 
+
 class Request(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
-    created_date = models.DateTimeField(auto_now_add=True)
-    status = models.CharField(max_length=20, default='Pending')
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    
+    created_by = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="requests_created"
+    )
+    assigned_to = models.ManyToManyField(
+        User, related_name="requests_assigned_to", blank=True
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=20, default="Pending")
+
     def __str__(self):
         return self.title
 
@@ -21,4 +28,4 @@ class Program(models.Model):
         return self.name
 
     def get_absolute_url(self):
-        return reverse('approvals:program_detail', args=[str(self.id)])
+        return reverse("approvals:program_detail", args=[str(self.id)])
